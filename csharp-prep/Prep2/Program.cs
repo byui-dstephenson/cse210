3506f613522be4a3d78c15eae608d4ce57b39162
Dave Stephenson <dstephenson@greeneis.com>
2023-01-02 20:11:23 -0700

    Completed Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Welcome to the CSE210 Grade Calculator!");
+        Console.WriteLine("Please enter your grade percentage (without the percent sign).");
+
+        string Grade = Console.ReadLine();
+        float GradePercentage = float.Parse(Grade);
+        string Letter = "";
+
+        if (GradePercentage >= 90)
+        {
+           Letter = "A";
+        }   
+        else if (GradePercentage >= 80)
+        {
+            Letter = "B";
+        }
+        else if (GradePercentage >= 70)
+        { 
+            Letter = "C";
+        }
+        else if (GradePercentage >= 60)
+        {
+            Letter = "D";
+        }
+        else 
+        {
+            Letter = "F";
+        }
+
+        Console.WriteLine($"You received an {Letter} in the class.");
+        if (GradePercentage >= 70)
+        {
+            
+            Console.WriteLine("This means you have passed!");
+        }
+        else
+        {
+            Console.WriteLine("This means you have failed the class.");
+            Console.WriteLine("Please consult with the teach to see how you can improve for the next time you take the class.");
+        }
+            
+
+
+
     }
 }
