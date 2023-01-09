71ad83e2dfd8993883c9ec80f17f9f865c87713c
Dave Stephenson <dstephenson@greeneis.com>
2023-01-09 06:09:45 -0700

    Completed Prep 3,4, and 5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        //int number = 4;
+        int guess = -5;
+        while (number != guess)  
+        {
+            Console.Write("What is the magic number? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (number > guess)
+            {
+                Console.WriteLine ("Hmm. That number is too low. Please guess something that is HIGHER.");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine ("Too bad. The number you guessed to too high. Please guess another number that is LOWER.");
+            }
+            else
+            {
+                Console.WriteLine ($"Bingo! You correctly guessed that {number} is the magic number!");    
+            }
+
+        }
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Create a list to hold the numbers
+        List<int> numbers = new List<int>();
+
+        //Have the user populate the list
+        Console.WriteLine($"\r\nPlease enter a number (one at a time) to added it to your number list. \r\nWhen finished, please enter a 0 to continue.");
+        int numberEntry = -1;
+        while (numberEntry !=0)
+        {
+            Console.Write("Number: "); string EnteredNumber = Console.ReadLine();
+            numberEntry = int.Parse(EnteredNumber);
+
+            if (numberEntry != 0)
+            {
+                numbers.Add(numberEntry);
+            }
+        }
+
+        //Sum the numbers from the list
+        int sum = 0;
+        foreach (int number in numbers)    
+        {
+            sum += number;
+        }
+        Console.WriteLine ($"\r\nThe sum of the numbers is: {sum}");
+
+        //Get the average of the numbers in the list
+        float avg = ((float)sum) /numbers.Count;
+        Console.WriteLine($"The average of the numbers is: {avg}");
+
+        //Get the highest number in the list
+        Console.WriteLine($"The highest number in the list is: " + numbers.Max());
+
+        //Get the lowest number in the list
+        Console.WriteLine($"The lowest number in the list is: " + numbers.Min());
     }
 }
